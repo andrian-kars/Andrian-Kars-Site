dd1f9fa4f6f6ac7c141f4222eaea884266c3e480
pmAndrian <66482484+pmAndrian@users.noreply.github.com>
2020-08-22 19:30:48 +0300

    build

---
+++ build/precache-manifest.ae58f1b7fb1d89927b963102847750a6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2350d1d1cb87c682013e6a60f2f15386",
+    "url": "/andrian-kars-site/index.html"
+  },
+  {
+    "revision": "934381ebccc6338e7578",
+    "url": "/andrian-kars-site/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ac960b151ba361b68cb6",
+    "url": "/andrian-kars-site/static/css/main.5675f6a8.chunk.css"
+  },
+  {
+    "revision": "934381ebccc6338e7578",
+    "url": "/andrian-kars-site/static/js/2.03f7af73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/andrian-kars-site/static/js/2.03f7af73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac960b151ba361b68cb6",
+    "url": "/andrian-kars-site/static/js/main.61fdf7f0.chunk.js"
+  },
+  {
+    "revision": "5d761865f4bdd93db550",
+    "url": "/andrian-kars-site/static/js/runtime-main.2e115234.js"
+  },
+  {
+    "revision": "56575512aca068ec6a199bea9b0dc57a",
+    "url": "/andrian-kars-site/static/media/andrian-kars.56575512.jpg"
+  },
+  {
+    "revision": "c16507408445685584a16f1f15b9ee69",
+    "url": "/andrian-kars-site/static/media/apollo.c1650740.jpg"
+  },
+  {
+    "revision": "b04900b6f9f171864ad07204e9baab1e",
+    "url": "/andrian-kars-site/static/media/brainbook.b04900b6.jpg"
+  },
+  {
+    "revision": "0b40dc536264927f065f937646f1f9e4",
+    "url": "/andrian-kars-site/static/media/contact-top-mobile.0b40dc53.jpg"
+  },
+  {
+    "revision": "bf0dd2abf5e0c55b93f18c661fd02e95",
+    "url": "/andrian-kars-site/static/media/contact-top.bf0dd2ab.jpeg"
+  },
+  {
+    "revision": "3c3e9c6ed439531e9ea6555c6bafeb71",
+    "url": "/andrian-kars-site/static/media/edit.3c3e9c6e.svg"
+  },
+  {
+    "revision": "d020eec2da26c1806221639393915a65",
+    "url": "/andrian-kars-site/static/media/home-main-mobile.d020eec2.jpg"
+  },
+  {
+    "revision": "a187d46978e82220dc0be6cec723b87d",
+    "url": "/andrian-kars-site/static/media/home-main.a187d469.jpg"
+  },
+  {
+    "revision": "aebf0d7310fc59a66a1e05e282d8a483",
+    "url": "/andrian-kars-site/static/media/leo-shop.aebf0d73.jpg"
+  },
+  {
+    "revision": "7ca6f860354f3e82f67fc9fbd2da2ea3",
+    "url": "/andrian-kars-site/static/media/like.7ca6f860.svg"
+  },
+  {
+    "revision": "41fb3d856b8c2cbafcdba326897304de",
+    "url": "/andrian-kars-site/static/media/mavic-2.41fb3d85.jpg"
+  },
+  {
+    "revision": "0cac6f507afff413c253cd30f5f6054e",
+    "url": "/andrian-kars-site/static/media/medal.0cac6f50.svg"
+  },
+  {
+    "revision": "364ed231247ae3e36cb6ba42b5dd276f",
+    "url": "/andrian-kars-site/static/media/organic.364ed231.jpg"
+  },
+  {
+    "revision": "ea9796efb51ec437664c0ca43ab59661",
+    "url": "/andrian-kars-site/static/media/osf-academy.ea9796ef.jpg"
+  },
+  {
+    "revision": "d444e5d3d7081de377626b294f971b18",
+    "url": "/andrian-kars-site/static/media/personal-site.d444e5d3.jpg"
+  },
+  {
+    "revision": "3d6b4ac148567299f08bc88779669dfa",
+    "url": "/andrian-kars-site/static/media/surface1.3d6b4ac1.jpg"
+  },
+  {
+    "revision": "e2ee8db4ac554ebdfa91852e5cc00236",
+    "url": "/andrian-kars-site/static/media/year.e2ee8db4.svg"
+  }
+]);
