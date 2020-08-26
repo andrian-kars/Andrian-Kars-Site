8aa5bacc306bf2df8d34609265c76093e39a1e0f
pmAndrian <66482484+pmAndrian@users.noreply.github.com>
2020-08-26 14:08:33 +0300

    small-bug-fixes

--- src/components/Portfolio/NewProject/NewProject.jsx
+++ src/components/Portfolio/NewProject/NewProject.jsx
@@ -5,11 +5,13 @@ const NewProject = (props) => {
     return (
         <div className={s.project}>
             <div style={props.ProjectImage} className={s.image}>
-                <div className={s.shades}></div>
-                <a className={s.popup} href={props.ProjectLink} target="_blank" rel="noopener noreferrer">
-                    <h2 className={s.heading}>{props.ProjectName}</h2>
-                    <p className={s.description}>{props.ProjectTechnologies}</p>
-                </a>
+                <div className={s.whrapper}>
+                    <div className={s.shades}></div>
+                    <a className={s.popup} href={props.ProjectLink} target="_blank" rel="noopener noreferrer">
+                        <h2 className={s.heading}>{props.ProjectName}</h2>
+                        <p className={s.description}>{props.ProjectTechnologies}</p>
+                    </a>
+                </div>
             </div>
         </div>
     )
